47178f70b860f62b28cd551e056820dc6a111d37
Baher-essam <44957599+Baher-essam@users.noreply.github.com>
2019-11-11 20:17:47 +0200

    Search & Update Products
    Realtime search for products and update it after add.

--- JS/index.js
+++ JS/index.js
@@ -6,8 +6,11 @@ var productCompanyInp= document.getElementById("prodcutCompany");
 var productDescInp= document.getElementById("prodcutDesc");
 var btn = document.getElementById("btn");
 var searchInp = document.getElementById("searchInp");
-
+var searchRow = document.getElementById("searchRow");
+var updateBtn = document.getElementById("update");
 var productsContainer;
+var currentIndex;
+
 
 if (localStorage.getItem("productsContainer") == null)
 {
@@ -25,27 +28,43 @@ searchInp.onkeyup = function()
     searchProducts(searchInp.value);
 }
 
+//Products Container
 function searchProducts(term)
 {
+    var searchCols ="";
     for(var i=0 ; i< productsContainer.length ;i++)
-    {
-        if(productsContainer[i].name == term)
+    {  
+        if(productsContainer[i].name.includes(term))
         {
-            console.log(productsContainer[i]);
-        } 
-    }
+            searchCols+=`<div class="col-md-3">
+            <div class="prodcut">
+                <h3>`+productsContainer[i].name+`</h3>
+                <p>`+productsContainer[i].desc+`</p>
+                <p class="text-danger">`+productsContainer[i].company+`</p>
+                <p class="text-info">`+productsContainer[i].price+`</p>
+            </div>
+        </div>`;
+        }
+    } 
+    searchRow.innerHTML= searchCols;
 }
 
-
+//add new product with the add btn product, display, and clear inputs after add
 btn.onclick = function()
 {   
-    addProduct();
-    displayData();
-    clearForm();
+    if(btn.innerHTML == "Add Product")
+    {
+        addProduct();
+        displayData();
+        clearForm();
+    }
+    else
+    {
+        updateProduct();
+        displayData();
+    }
 }
-
-
-
+//adding product to local storge 
 function addProduct()
 {
     var product = 
@@ -55,29 +74,49 @@ function addProduct()
         company : productCompanyInp.value,
         desc :    productDescInp.value
     }
-
     productsContainer.push(product);
-    localStorage.setItem("productsContainer",JSON.stringify(productsContainer))
-   
+    localStorage.setItem("productsContainer",JSON.stringify(productsContainer));
 }
 
 //adding products after click add btn
 function displayData() 
 {
     var temp = "";
-    for (var i = 0 ; i <productsContainer.length ; i++)
+    for (var i = 0 ; i < productsContainer.length ; i++)
     {
         temp+=` <div class="col-md-3">
-        <div class="prodcut">
+        <div class="prodcut my-3">
             <h3>`+productsContainer[i].name+`</h3>
             <p>`+productsContainer[i].desc+`</p>
             <p class="text-danger">`+productsContainer[i].company+`</p>
             <p class="text-info">`+productsContainer[i].price+`</p>
-            <button id="delete" class="btn btn-danger" onclick="deleteProdcut(`+i+`)">delete</button>
+            <button class="btn btn-danger mr-2" onclick="deleteProdcut(`+i+`)">Delete</button>
+            <button class="btn btn-info" onclick="setForm(`+i+`)">Update</button>
         </div>
     </div>`;
     }
     document.getElementById("cols").innerHTML=temp;
+}
+
+//set product into inputs to update it
+function setForm(i)
+{
+    productNameInp.value =  productsContainer[i].name;
+    productPriceInp.value =  productsContainer[i].price;
+    productCompanyInp.value =  productsContainer[i].company;
+    productDescInp.value =  productsContainer[i].desc;
+    btn.innerHTML = "Update Product";
+    currentIndex = i;
+}
+
+function updateProduct()
+{
+    productsContainer[currentIndex].name = productNameInp.value;
+    productsContainer[currentIndex].price = productPriceInp.value;
+    productsContainer[currentIndex].company = productCompanyInp.value;
+    productsContainer[currentIndex].desc = productDescInp.value;
+    btn.innerHTML = "Add Product";
+    localStorage.setItem("productsContainer",JSON.stringify(productsContainer));
 }
 
 //clear inputs after adding prodcuts
@@ -98,3 +137,4 @@ function deleteProdcut(id)
     localStorage.setItem("productsContainer",JSON.stringify(productsContainer))
     displayData();
 }
+//##
